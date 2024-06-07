e0bcc9b508e1dca3fa3abf20669c47ba943b3051
Mark John <160160952+IamGotu@users.noreply.github.com>
2024-06-07 23:31:23 +0800

    fixing the looks of privacy and condition file

--- Terms&Condition.php
+++ Terms&Condition.php
@@ -1,22 +1,49 @@
-<!DOCTYPE html>
-<html>
-    <head>
+<?php
+include('includes/header.php');
+?>
 
-        <title>Terms & Condition</title>
-        <link rel="stylesheet" href="Stylesheet.css">
-        <!-- Check if the 'error' parameter is set in the URL and display error message-->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-fluid">
 
-    </head>
-    <body>  
-        <div class="container">
+<!-- Main content -->
+<section class="content text-justify col-sm-12">
+    <div class="container-fluid">
+        <div class="container-fluid">
             <!-- Form for user login, submits data to signupcode.php using POST method -->
-            <form action="signupcode.php" method="post" class="mt-5">
+            <form class="md-5">
 
-                <h2 class="mb-4">Terms & Conditions</h2>
-                <p class="text-center">Providing our Service requires collecting and using your information.</p>
-                <?php include('footer.php'); ?>
+                <h2>Terms of Service</h2>
+    
+                <p >Please read these terms of service carefully before using the website operated by the developer.</p>
+                
+                <h2>Conditions of Use</h2>
+                
+                <p>We will provide services to you, which are subject to the conditions stated below in this document. Every time you visit this website, use its services or make a purchase, you accept the following conditions. This is why we urge you to read them carefully.</p>
+                
+                <h2>Privacy Policy</h2>
+                
+                <p>Before you continue using our website we advise you to read our privacy policy regarding our user data collection. It will help you better understand our practices.</p>
+                                
+                <h2>Communications</h2>
+                
+                <p>The entire communication with us is electronic. Every time you send us an email or visit our website, you are going to be communicating with us. You hereby consent to receive communications from us. If you subscribe to the news on our website, you are going to receive regular emails from us. We will continue to communicate with you by posting news and notices on our website and by sending you emails. You also agree that all notices, disclosures, agreements and other communications we provide to you electronically meet the legal requirements that such communications be in writing.</p>
+                
+                <h2>Applicable Law</h2>
+                
+                <p>By visiting this website, you agree that the laws of the Philippines, without regard to principles of conflict laws, will govern these terms of service, or any dispute of any sort that might come between the website
+                     
+                <h2>Disputes</h2>
+                
+                <p>Any dispute related in any way to your visit to this website or to products you purchase from us shall be arbitrated by state or federal court [Your Location] and you consent to exclusive jurisdiction and venue of such courts.</p>
+                
+                <p>Please read our <a href="Privacy_Policy.php">Privacy Policy</a></p>
+
             </form>
-    </body>
-</html>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
 
+</div>
+
+<?php include('includes/script.php'); ?>
+<?php include('includes/footer.php'); ?>
--- privacy_policy.php
+++ privacy_policy.php
@@ -1,56 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Privacy Policy</title>
-</head>
-<body>
-    <h1>Privacy Policy</h1>
+<?php
+include('includes/header.php');
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-fluid">
+
+<!-- Main content -->
+<section class="content text-justify col-sm-12">
+    <div class="container-fluid">
+        <div class="container-fluid">
+            <!-- Form for user login, submits data to signupcode.php using POST method -->
+            <form class="md-5">
+
+                <h2>Privacy Policy</h2>
     
-    <p>This privacy policy ("policy") will help you understand how Shought ("us", "we", "our") uses and protects the data you provide to us when you visit and use Shought ("website", "service").</p>
-    
-    <p>We reserve the right to change this policy at any given time, of which you will be promptly updated. If you want to make sure that you are up to date with the latest changes, we advise you to frequently visit this page.</p>
-    
-    <h2>What User Data We Collect</h2>
-    
-    <p>When you visit the website, we may collect the following data:</p>
-    <ul>
-        <li>Your IP address.</li>
-        <li>Your contact information and email address.</li>
-        <li>Other information such as interests and preferences.</li>
-        <li>Data profile regarding your online behavior on our website.</li>
-    </ul>
-    
-    <h2>Why We Collect Your Data</h2>
-    
-    <p>We are collecting your data for several reasons:</p>
-    <ul>
-        <li>To better understand your needs.</li>
-        <li>To improve our services and products.</li>
-        <li>To send you promotional emails containing the information we think you will find interesting.</li>
-        <li>To contact you to fill out surveys and participate in other types of market research.</li>
-        <li>To customize our website according to your online behavior and personal preferences.</li>
-    </ul>
-    
-    <h2>Safeguarding and Securing the Data</h2>
-    
-    <p>Shought is committed to securing your data and keeping it confidential. Shought has done all in its power to prevent data theft, unauthorized access, and disclosure by implementing the latest technologies and software, which help us safeguard all the information we collect online.</p>
-    
-    <h2>Links to Other Websites</h2>
-    
-    <p>Our website contains links that lead to other websites. If you click on these links Shought is not held responsible for your data and privacy protection. Visiting those websites is not governed by this privacy policy agreement. Make sure to read the privacy policy documentation of the website you go to from our website.</p>
-    
-    <h2>Restricting the Collection of your Personal Data</h2>
-    
-    <p>At some point, you might wish to restrict the use and collection of your personal data. You can achieve this by doing the following:</p>
-    <ul>
-        <li>When you are filling the forms on the website, make sure to check if there is a box which you can leave unchecked, if you don't want to disclose your personal information.</li>
-        <li>If you have already agreed to share your information with us, feel free to contact us via email and we will be more than happy to change this for you.</li>
-    </ul>
-    
-    <p>Shought will not lease, sell or distribute your personal information to any third parties, unless we have your permission. We might do so if the law forces us. Your personal information will be used when we need to send you promotional materials if you agree to this privacy policy.</p>
-    
-    <p>Please read our <a href="terms_of_service.php">Terms of Service</a></p>
-</body>
-</html>
+                <p>This privacy policy will help you understand how the website uses and protects the data you provide to us when you visit and use the website.</p>
+                
+                <p>We reserve the right to change this policy at any given time, of which you will be promptly updated. If you want to make sure that you are up to date with the latest changes, we advise you to frequently visit this page.</p>
+                
+                <h3>What User Data We Collect</h3>
+                
+                <p>When you visit the website, we may collect the following data:</p>
+                <ul>
+                    <li>Your IP address.</li>
+                    <li>Your contact information and email address.</li>
+                    <li>Other information such as interests and preferences.</li>
+                    <li>Data profile regarding your online behavior on our website.</li>
+                </ul>
+                
+                <h3>Why We Collect Your Data</h3>
+                
+                <p>We are collecting your data for several reasons:</p>
+                <ul>
+                    <li>To better understand your needs.</li>
+                    <li>To improve our services and products.</li>
+                    <li>To send you promotional emails containing the information we think you will find interesting.</li>
+                    <li>To contact you to fill out surveys and participate in other types of market research.</li>
+                    <li>To customize our website according to your online behavior and personal preferences.</li>
+                </ul>
+                
+                <h3>Safeguarding and Securing the Data</h3>
+                
+                <p>The website is committed to securing your data and keeping it confidential. The website has done all in its power to prevent data theft, unauthorized access, and disclosure by implementing the latest technologies and software, which help us safeguard all the information we collect online.</p>
+                
+                <h3>Links to Other Websites</h3>
+                
+                <p>Our website contains links that lead to other websites. If you click on these links the website is not held responsible for your data and privacy protection. Visiting those websites is not governed by this privacy policy agreement. Make sure to read the privacy policy documentation of the website you go to from our website.</p>
+                
+                <h3>Restricting the Collection of your Personal Data</h3>
+                
+                <p>At some point, you might wish to restrict the use and collection of your personal data. You can achieve this by doing the following:</p>
+                <ul>
+                    <li>When you are filling the forms on the website, make sure to check if there is a box which you can leave unchecked, if you don't want to disclose your personal information.</li>
+                    <li>If you have already agreed to share your information with us, feel free to contact us via email and we will be more than happy to change this for you.</li>
+                </ul>
+                
+                <p>The website will not lease, sell or distribute your personal information to any third parties, unless we have your permission. We might do so if the law forces us. Your personal information will be used when we need to send you promotional materials if you agree to this privacy policy.</p>
+                
+                <p>Please read our <a href="Terms&Condition.php">Terms of Service</a></p>
+
+            </form>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+</div>
+
+<?php include('includes/script.php'); ?>
+<?php include('includes/footer.php'); ?>
