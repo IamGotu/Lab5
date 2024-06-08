2ec538928f2fba86c5cb90a267c1829d92da0b9a
Mark John <markjohnjopia1@gmail.com>
2024-06-08 12:35:23 +0800

    changing folder name to Google_login

--- Loginform.php
+++ Loginform.php
@@ -7,7 +7,7 @@ if(isset($_SESSION['auth']))
     header('Location: Dashboard.php');
     exit(0);
 }
-
+require_once ('Google_login/vendor/autoload.php');
 ?>
 
     <div class="section">
@@ -45,6 +45,36 @@ if(isset($_SESSION['auth']))
 
                                     <hr>
 
+                                    <?php
+                                    // init configuration
+                                    $clientID = '147613768576-2bund5num0s4eams0tglrgav3vauu79j.apps.googleusercontent.com';
+                                    $clientSecret = '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';
+                                    $redirectUri = 'http://localhost:3000/Dashboard.php';
+
+                                    // create Client Request to access Google API
+                                    $client = new Google_Client();
+                                    $client->setClientId($clientID);
+                                    $client->setClientSecret($clientSecret);
+                                    $client->setRedirectUri($redirectUri);
+                                    $client->addScope("email");
+                                    $client->addScope("profile");
+
+                                    // authenticate code from Google OAuth Flow
+                                    if (isset($_GET['code'])) {
+                                    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+                                    $client->setAccessToken($token['access_token']);
+
+                                    // get profile info
+                                    $google_oauth = new Google_Service_Oauth2($client);
+                                    $google_account_info = $google_oauth->userinfo->get();
+                                    $email =  $google_account_info->email;
+                                    $name =  $google_account_info->name;
+
+                                    // now you can use this profile info to create account in your website and make user logged in.
+                                    } else {
+                                    echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+                                    }
+                                    ?>
                             </form>                                
                                 <p>Don't have an account? <a href="signupform.php" class="btn-sm">Sign Up</a></p>
                             </div>
