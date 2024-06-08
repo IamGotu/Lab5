35f5d3b5eddd21c51d516a3b5c72099670deb659
Mark John <markjohnjopia1@gmail.com>
2024-06-08 12:24:07 +0800

    removing unused code and file

--- Loginform.php
+++ Loginform.php
@@ -45,69 +45,7 @@ if(isset($_SESSION['auth']))
 
                                     <hr>
 
-                            </form>
-
-                                <!-- Sign In With Google button with HTML data attributes API -->
-                                <script src="https://accounts.google.com/gsi/client" async></script>
-
-                                <script>
-                                    // Credential response handler function
-                                    function handleCredentialResponse(response){
-                                        // Post JWT token to server-side
-                                        fetch("auth_init.php", {
-                                            method: "POST",
-                                            headers: { "Content-Type": "application/json" },
-                                            body: JSON.stringify({ request_type:'user_auth', credential: response.credential }),
-                                        })
-                                        .then(response => response.json())
-                                        .then(data => {
-                                            if(data.status == 1){
-                                                let responsePayload = data.pdata;
-
-                                                // Display the user account data
-                                                let profileHTML = '<h3>Welcome '+responsePayload.given_name+'! <a href="javascript:void(0);" onclick="signOut('+responsePayload.sub+');">Sign out</a></h3>';
-                                                profileHTML += '<img src="'+responsePayload.picture+'"/><p><b>Auth ID: </b>'+responsePayload.sub+'</p><p><b>Name: </b>'+responsePayload.name+'</p><p><b>Email: </b>'+responsePayload.email+'</p>';
-                                                document.getElementsByClassName("pro-data")[0].innerHTML = profileHTML;
-                                                
-                                                document.querySelector("#btnWrap").classList.add("hidden");
-                                                document.querySelector(".pro-data").classList.remove("hidden");
-                                            }
-                                        })
-                                        .catch(console.error);
-                                    }
-
-                                    // Sign out the user
-                                    function signOut(authID) {
-                                        document.getElementsByClassName("pro-data")[0].innerHTML = '';
-                                        document.querySelector("#btnWrap").classList.remove("hidden");
-                                        document.querySelector(".pro-data").classList.add("hidden");
-                                    }    
-                                </script>
-
-                                <!-- Sign In With Google button with HTML data attributes API -->
-                                <div id="g_id_onload"
-                                    data-client_id="147613768576-7g0621s87copo0e98i6ht9rdtiru9q3f.apps.googleusercontent.com"
-                                    data-context="signin"
-                                    data-ux_mode="popup"
-                                    data-callback="handleCredentialResponse"
-                                    data-auto_prompt="false">
-                                </div>
-
-                                <div class="g_id_signin"
-                                    data-type="standard"
-                                    data-shape="rectangular"
-                                    data-theme="outline"
-                                    data-text="signin_with"
-                                    data-size="large"
-                                    data-logo_alignment="left">
-                                </div>
-
-                                <!-- Display the user's profile info -->
-                                <div class="pro-data hidden"></div>
-                                <div class="text-center">
-
-                                <hr>
-                                
+                            </form>                                
                                 <p>Don't have an account? <a href="signupform.php" class="btn-sm">Sign Up</a></p>
                             </div>
                         </div> 
